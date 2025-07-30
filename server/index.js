56a8c16010cf4e13dde8d00d077b117b398d77f3
GanpatiIIMS <iimshillongganpati@gmail.com>
2025-07-30 14:18:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://ganpatiiims.github.io"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 // ---------- Send Email Route ----------
