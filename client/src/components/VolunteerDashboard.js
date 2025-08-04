a3f93338cf3ec2696ad86e7dfd86afcb12667e0f
GanpatiIIMS <iimshillongganpati@gmail.com>
2025-08-05 03:30:53 +0530

    Update VolunteerDashboard.js

--- client/src/components/VolunteerDashboard.js
+++ client/src/components/VolunteerDashboard.js
@@ -479,7 +479,7 @@ function VolunteerDashboard({ user }) {
       // await new Promise(resolve => setTimeout(resolve, emailDelay));
 
       //await fetch("http://localhost:8080/send-email", {
-      await fetch("https://varghanicollectiontracker.onrender.com/send-email",{
+      await fetch("https://varghani.onrender.com/send-email",{
         method: "POST",                                                                       
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -593,4 +593,5 @@ function VolunteerDashboard({ user }) {
 }
 
 export default VolunteerDashboard;
+
 
